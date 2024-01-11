9c61756284da86e2ac2490ada042cfe6ad9a0434
lumine <hakupisi>
2024-01-11 22:34:39 +0100

    Corretto l'evidenziazione delle miniature

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ set_error_handler(
   fn ($errno, $errstr, $errfile, $errline) => throw new ErrorException($errstr, $errno, 0, $errfile, $errline)
 );
 
+const TEXT_MODE = 'Content-Type: text/plain; charset=utf-8';
+
 // ========================================================================================================================
 // QOL
 // ========================================================================================================================
@@ -28,8 +30,8 @@ class Stream {
     return $this->set(array_map($function, array_keys($this->collection), $this->collection));
   }
 
-  public function filter($function): Stream {
-    return $this->set(array_filter($this->collection, $function));
+  public function filter($function, $mode = 0): Stream {
+    return $this->set(array_filter($this->collection, $function, $mode));
   }
 
   public function get(): array {
@@ -264,12 +266,12 @@ try {
 
   $diff = array_diff($files, $_SESSION['files']);
   if (count($diff) > 0) {
-    $max = max(array_keys($_SESSION['files']));
+    $max = max(array_keys($_SESSION['files'])) + 1;
     $diff = array_combine(range($max, $max + count($diff) - 1), $diff);
     $_SESSION['files'] = $diff + array_intersect($_SESSION['files'], $files);
 
     $_SESSION['associazioni'] = stream($_SESSION['associazioni'])
-      ->filter(fn ($file) => in_array($file, $_SESSION['files']))
+      ->filter(fn ($file) => array_key_exists($file, $_SESSION['files']), ARRAY_FILTER_USE_KEY)
       ->get();
   }
 } catch (Exception | ValueError) {
